cad11567efd3dbad4bd95d2f30db0a21fe79ca14
Chris Hokamp <chris.hokamp@gmail.com>
2014-03-10 12:13:59 +0000

    i want to use eclipse, not netbeans

--- runBB.sh
+++ runBB.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-java -Xmx1g -XX:+UseConcMarkSweepGC -classpath build/classes:lib/commons-cli-1.2.jar:lib/stanford-postagger.jar:lib/BerkeleyParser-1.7.jar shef.mt.enes.FeatureExtractorSimple -lang english spanish -input input/source.en input/target.es -mode bb -config config/wmt14-config_en-es.properties
+
+
+#java -Xmx1g -XX:+UseConcMarkSweepGC -classpath build/classes:lib/commons-cli-1.2.jar:lib/stanford-postagger.jar:lib/BerkeleyParser-1.7.jar shef.mt.enes.FeatureExtractorSimple -lang english spanish -input input/source.en input/target.es -mode bb -config config/wmt14-config_en-es.properties
+java -Xmx1g -XX:+UseConcMarkSweepGC -classpath bin:lib/commons-cli-1.2.jar:lib/stanford-postagger.jar:lib/BerkeleyParser-1.7.jar shef.mt.enes.FeatureExtractorSimple -lang english spanish -input input/source.en input/target.es -mode bb -config config/wmt14-config_en-es.properties
